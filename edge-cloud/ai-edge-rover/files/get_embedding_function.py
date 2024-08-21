e371fd907c640671afba6efa76b46886da0e2d04
Kamryn Vinson <68861214+kamryn-v@users.noreply.github.com>
2024-08-21 10:01:28 -0400

    Revert "OCI Edge Cloud - Enable GenAI from Cloud to the Edge: ai-edge-rover" (#405)
    Revert "OCI Edge Cloud - Enable GenAI from Cloud to the Edge: ai-edge-rover (…"
    
    This reverts commit 7ad488855ebcda365939901b4a269744b666d91b.

--- edge-cloud/ai-edge-rover/files/get_embedding_function.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-
-def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    return embeddings
